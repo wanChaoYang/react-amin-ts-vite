1f30cf9c8b2d952f69d3ffd8281dff8b6c8d9d48
wanChaoYang <1054895175@qq.com>
2023-04-13 22:30:46 +0800

    logo的优化

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { HashRouter } from "react-router-dom";
 import GetRouters from "./router";
 import { BeforeRouter } from "./components/index";
+import "./App.scss";
 function App() {
   return (
     <HashRouter>
--- src/view/login/index.tsx
+++ src/view/login/index.tsx
@@ -1,13 +1,13 @@
 import "./index.scss";
-import logo from "@/assets/react.svg";
+import reactLogo from "@/assets/react.svg";
 import LoginForm from "./components/LoginForm";
 function Login() {
   return (
     <div className="login-container">
       <div className="login-wrap">
         <div className="login-form">
-          <img src={logo} alt="logo" />
-          <h1>React+Hook+Admin</h1>
+          <img className="logo" src={reactLogo} alt="logo" />
+          <h2>React+Hook+Admin</h2>
           <LoginForm />
         </div>
       </div>
